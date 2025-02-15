8cb55bcabb0d2020cf9bd5b1f14740dbe6be1be1
jehad <rexovif974@gekme.com>
2025-02-15 15:42:23 +0200

    second commit jehad shurrab

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Carbon\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,7 +19,7 @@ Route::get('/about', function () {
     //return view('about', ['name' => $name]);
     //return view('about')->with('name', $name);
 
-    return view('about', compact('name' , 'departments'));
+    return view('about', compact('name', 'departments'));
 });
 
 Route::post('/about', function () {
@@ -29,4 +30,12 @@ Route::post('/about', function () {
         '3' => 'Sales',
     ];
     return view('about', compact('name', 'departments'));
+});
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return view('tasks');
 });
